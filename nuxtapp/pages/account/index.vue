68e4d97f477f2f8f71472af57aa41e990998efbf
Mchtsz <mhatland206@gmail.com>
2024-05-10 15:08:52 +0200

    clean acc info look

--- nuxtapp/pages/account/index.vue
+++ nuxtapp/pages/account/index.vue
@@ -2,7 +2,7 @@
 
 <template>
   <section>
-    <h1>Contact information</h1>
+    <h1>Account information</h1>
     <form action="/editContactinfo" method="POST">
       <input type="text" name="firstname" placeholder="firstname" />
       <input type="text" name="lastname" placeholder="lastname" />
@@ -10,7 +10,7 @@
       <input type="text" name="phone" placeholder="phone" />
       <input type="text" name="address" placeholder="address" />
       <input type="text" name="city" placeholder="city" />
-      <input type="submit" value="Submit" />
+      <input type="submit" value="Update" />
     </form>
   </section>
 </template>
@@ -23,7 +23,6 @@ section {
   justify-content: flex-start; // Align items to the start
   height: 500px;
   width: 800px;
-  align-items: center;
   margin: 10vh auto;
   padding: 2rem 4rem;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -37,6 +36,7 @@ section {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     align-items: center;
+    padding-top: 2rem;
     gap: 1rem;
     width: 100%;
     height: 100%;
@@ -54,6 +54,8 @@ section {
         padding: 1rem;
         grid-column: span 2; // Make the button span both columns
         justify-self: center; // Center the button
+        background-color: #000;
+        color: #fff;
         border-radius: 1rem;
         border: none;
         margin-top: 1rem;
