e166896a07b106fb9778bbd2d8031f7a7c3ffb28
Mchtsz <mhatland206@gmail.com>
2024-05-15 14:33:56 +0200

    works

--- nuxtapp/pages/admin/index.vue
+++ nuxtapp/pages/admin/index.vue
@@ -1,15 +1,4 @@
 <script setup lang=ts>
-const fetchData = async () => {
-  try {
-    const response = await fetch("http://localhost:4000/api/users");
-    const data = await response.json();
-    console.log(data)
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-fetchData();
 </script>
 
 <template>
--- nuxtapp/pages/shoes.vue
+++ nuxtapp/pages/shoes.vue
@@ -4,6 +4,7 @@
 
 <template>
     <h1>Welcome to shoes page</h1>
+    <img src="https://i.imgur.com/B6WoOer.jpg" alt="sko">
 </template>
 
 <style scoped lang=scss>
